e39ad40e0a2f18b5495c2b59bd960fec42cb3f65
glee1 <gene@Genes-MacBook-Pro.local>
2017-04-20 15:10:38 -0700

    Updated HW8

---
+++ Homework/HW8/HW8/scp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+scp ~/Desktop/HW8/server.js pi@192.168.88.249:~/Desktop/CS489_HW8/
+scp ~/Desktop/HW8/getClient.html pi@192.168.88.249:~/Desktop/CS489_HW8/
+scp ~/Desktop/HW8/postClient.html pi@192.168.88.249:~/Desktop/CS489_HW8/
